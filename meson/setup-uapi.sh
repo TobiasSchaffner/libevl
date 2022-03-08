TobiasSchaffner/libevl

commit c70a9d9d536aefcf6574328aaac0fcc86f9ab0f7

build: tighten consistency check for uapi
Signed-off-by: Philippe Gerum <rpm@xenomai.org>

meson/setup-uapi.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-if test \! -d $UAPI; then
-    echo "meson: path given to -Duapi does not look right"
+if test \! -d $UAPI/include/uapi/evl; then
+    echo "meson: path given to -Duapi does not look right ($UAPI/include/uapi/evl is missing)"
     exit 1
 fi
 

commit 6b64604badf444f01711a3f5be067bd219782ca4

meson: allow use of installed uapi headers
Commit c70a9d9d536aefcf6574328aaac0fcc86f9ab0f7 added a test for
$UAPI/include/uapi/evl that was meant to ensure the given $UAPI path was
correct.  That test meant that $UAPI had to point to a linux-evl source
directory and could not point to installed headers in e.g. /usr/include.
The script had previously allowed the use of installed headers, using
the existence of $UAPI/Kbuild as an heuristic to decide whether $UAPI
pointed to linux-evl source or to installed headers.

Rewrite the script to check for the existence of all three required
directories in either location.  This allows the use of installed
headers again.

Fixes: c70a9d9 (build: tighten consistency check for uapi)
Signed-off-by: Evan Gates <evan.gates@gmail.com>

meson/setup-uapi.sh
@@ -1,19 +1,19 @@
 #! /bin/sh
 
-if test \! -d $UAPI/include/uapi/evl; then
-    echo "meson: path given to -Duapi does not look right ($UAPI/include/uapi/evl is missing)"
-    exit 1
+if test -r $UAPI/Kbuild; then
+	set \
+		$UAPI/arch/$ARCH/include/uapi/asm \
+		$UAPI/include/uapi/asm-generic \
+		$UAPI/include/uapi/evl
+else
+	set $UAPI/asm $UAPI/asm-generic $UAPI/evl
 fi
+
+for d do
+	test -d $d && continue
+	echo "meson: path given to -Duapi does not look right ($d is missing)"
+	exit 1
+done
 
 mkdir -p $O_UAPI
-rm -f $O_UAPI/asm $O_UAPI/asm-generic $O_UAPI/evl
-
-if test -r $UAPI/Kbuild; then
-    ln -s $UAPI/arch/$ARCH/include/uapi/asm $O_UAPI/asm
-    ln -s $UAPI/include/uapi/asm-generic $O_UAPI/asm-generic
-    ln -s $UAPI/include/uapi/evl $O_UAPI/evl
-elif test \! -L	$O_UAPI/asm; then
-    ln -s $UAPI/asm $O_UAPI/asm
-    ln -s $UAPI/asm-generic $O_UAPI/asm-generic
-    ln -s $UAPI/evl $O_UAPI/evl
-fi
+for d do ln -sf $d $O_UAPI/; done