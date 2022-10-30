49c6c39e0844084c0a1a7aa569e25651be0ec094
Philippe Gerum <rpm@xenomai.org>
2022-10-30 15:03:09 +0100

    tests: monitor-pi-deadlock: check for proper deboosting on error
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- tests/monitor-pi-deadlock.c
+++ tests/monitor-pi-deadlock.c
@@ -5,75 +5,126 @@
  */
 
 #include <sys/types.h>
-#include <time.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <evl/thread.h>
 #include <evl/mutex.h>
-#include <evl/clock.h>
 #include <evl/sem.h>
 #include "helpers.h"
 
 #define LOW_PRIO	1
-#define MID_PRIO	2
+#define MEDIUM_PRIO	2
 #define HIGH_PRIO	3
 
 struct test_context {
 	struct evl_mutex lock_a;
 	struct evl_mutex lock_b;
+	struct evl_mutex lock_c;
 	struct evl_sem start;
 	struct evl_sem sync;
 };
 
-static void *deadlocking_thread(void *arg)
+static bool check_priority(int tfd, int prio)
+{
+	struct evl_thread_state statebuf;
+	int ret;
+
+	__Tcall_assert(ret, evl_get_state(tfd, &statebuf));
+
+	return statebuf.eattrs.sched_policy == SCHED_FIFO &&
+		statebuf.eattrs.sched_priority == prio;
+}
+
+static void *thread_c(void *arg)
 {
 	struct test_context *p = arg;
 	int ret, tfd;
 
-	__Tcall_assert(tfd, evl_attach_self("monitor-pi-dlk-B:%d", getpid()));
+	__Tcall_assert(tfd, evl_attach_self("monitor-pi-dlk-C:%d", getpid()));
 	/*
 	 * Disable WOLI in case CONFIG_EVL_DEBUG_WOLI is set, as we
 	 * are about to sleep while holding a mutex.
 	 */
 	__Tcall_assert(ret, evl_clear_thread_mode(tfd, T_WOLI, NULL));
-	__Tcall_assert(ret, evl_lock_mutex(&p->lock_b));
+	__Tcall_assert(ret, evl_lock_mutex(&p->lock_c));
 	__Tcall_assert(ret, evl_put_sem(&p->sync));
+
+	/* Wait for start call. */
 	__Tcall_assert(ret, evl_get_sem(&p->start));
+	/* We should have inherited thread_a effective priority. */
+	__Texpr_assert(check_priority(tfd, HIGH_PRIO));
+
 	__Fcall_assert(ret, evl_lock_mutex(&p->lock_a)); /* ABBA deadlock */
 	__Texpr_assert(ret == -EDEADLK);
-	__Tcall_assert(ret, evl_unlock_mutex(&p->lock_b));
+	__Tcall_assert(ret, evl_unlock_mutex(&p->lock_c));
+	/* We should have been deboosted to our initial priority. */
+	__Texpr_assert(check_priority(tfd, LOW_PRIO));
 
 	return NULL;
 }
 
-static void *starter_thread(void *arg)
+static void *thread_b(void *arg)
 {
 	struct test_context *p = arg;
-	int tfd, ret;
+	int ret, tfd;
 
-	__Tcall_assert(tfd, evl_attach_self("monitor-pi-dlk-S:%d", getpid()));
-	__Tcall_assert(ret, evl_get_sem(&p->sync));
-	__Tcall_assert(ret, evl_put_sem(&p->start));
+	__Tcall_assert(tfd, evl_attach_self("monitor-pi-dlk-B:%d", getpid()));
+	__Tcall_assert(ret, evl_clear_thread_mode(tfd, T_WOLI, NULL));
+	__Tcall_assert(ret, evl_lock_mutex(&p->lock_b));
+	__Tcall_assert(ret, evl_put_sem(&p->sync));
+
+	/* Wait for start call. */
+	__Tcall_assert(ret, evl_get_sem(&p->start));
+	/* We should have inherited thread_a effective priority. */
+	__Texpr_assert(check_priority(tfd, HIGH_PRIO));
+
+	/* Wait for thread_c to release. */
+	__Tcall_assert(ret, evl_lock_mutex(&p->lock_c));
+	__Tcall_assert(ret, evl_unlock_mutex(&p->lock_c));
+
+	__Tcall_assert(ret, evl_unlock_mutex(&p->lock_b));
+	/* We should have been deboosted to our initial priority. */
+	__Texpr_assert(check_priority(tfd, MEDIUM_PRIO));
+
+	return NULL;
+}
+
+static void *thread_a(void *arg)
+{
+	struct test_context *p = arg;
+	int ret, tfd;
+
+	__Tcall_assert(tfd, evl_attach_self("monitor-pi-dlk-A:%d", getpid()));
+	__Tcall_assert(ret, evl_clear_thread_mode(tfd, T_WOLI, NULL));
+	__Tcall_assert(ret, evl_lock_mutex(&p->lock_a));
+	__Tcall_assert(ret, evl_put_sem(&p->sync));
+
+	/* Wait for start call. */
+	__Tcall_assert(ret, evl_get_sem(&p->start));
+	/* Our effective priority should not have changed. */
+	__Texpr_assert(check_priority(tfd, HIGH_PRIO));
+
+	/* Wait for thread_b to release. */
+	__Tcall_assert(ret, evl_lock_mutex(&p->lock_b));
+	__Texpr_assert(check_priority(tfd, HIGH_PRIO));
+	__Tcall_assert(ret, evl_unlock_mutex(&p->lock_b));
+
+	__Tcall_assert(ret, evl_unlock_mutex(&p->lock_a));
+	__Texpr_assert(check_priority(tfd, HIGH_PRIO));
 
 	return NULL;
 }
 
 int main(int argc, char *argv[])
 {
-	pthread_t deadlocker, starter;
-	struct sched_param param;
+	pthread_t t_a, t_b, t_c;
 	int tfd, gfd, sfd, ret;
 	struct test_context c;
 	char *name;
 
-	param.sched_priority = MID_PRIO;
-	__Texpr_assert(pthread_setschedparam(pthread_self(),
-				SCHED_FIFO, &param) == 0);
-
-	/* EVL inherits the inband scheduling params upon attachment. */
-	__Tcall_assert(tfd, evl_attach_self("monitor-pi-dlk-A:%d", getpid()));
+	__Tcall_assert(tfd, evl_attach_self("monitor-pi-dlk-main:%d", getpid()));
 
 	name = get_unique_name(EVL_MONITOR_DEV, 0);
 	__Tcall_assert(gfd, evl_new_mutex(&c.lock_a, name)); /* PI-implicit form. */
@@ -82,26 +133,42 @@ int main(int argc, char *argv[])
 	__Tcall_assert(gfd, evl_new_mutex(&c.lock_b, name));
 
 	name = get_unique_name(EVL_MONITOR_DEV, 2);
-	__Tcall_assert(sfd, evl_new_sem(&c.sync, name));
+	__Tcall_assert(gfd, evl_new_mutex(&c.lock_c, name));
 
 	name = get_unique_name(EVL_MONITOR_DEV, 3);
+	__Tcall_assert(sfd, evl_new_sem(&c.sync, name));
+
+	name = get_unique_name(EVL_MONITOR_DEV, 4);
 	__Tcall_assert(sfd, evl_new_sem(&c.start, name));
 
-	new_thread(&starter, SCHED_FIFO, LOW_PRIO, starter_thread, &c);
-	new_thread(&deadlocker, SCHED_FIFO, HIGH_PRIO, deadlocking_thread, &c);
+	new_thread(&t_a, SCHED_FIFO, HIGH_PRIO, thread_a, &c);
+	new_thread(&t_b, SCHED_FIFO, MEDIUM_PRIO, thread_b, &c);
+	new_thread(&t_c, SCHED_FIFO, LOW_PRIO, thread_c, &c);
 
+	/* Wait for all threads to synchronize with us. */
 	__Tcall_assert(ret, evl_get_sem(&c.sync));
-	__Tcall_assert(ret, evl_lock_mutex(&c.lock_a));
-	__Tcall_assert(ret, evl_put_sem(&c.sync));
-	__Tcall_assert(ret, evl_lock_mutex(&c.lock_b));
-	__Tcall_assert(ret, evl_unlock_mutex(&c.lock_a));
-	__Texpr_assert(pthread_join(deadlocker, NULL) == 0);
-	__Texpr_assert(pthread_join(starter, NULL) == 0);
+	__Tcall_assert(ret, evl_get_sem(&c.sync));
+	__Tcall_assert(ret, evl_get_sem(&c.sync));
+
+	/* Unblock A, which owns lock_a, attempts to lock lock_b. */
+	__Tcall_assert(ret, evl_put_sem(&c.start));
+	/* Unblock B, which owns lock_b, attempts to lock lock_c. */
+	__Tcall_assert(ret, evl_put_sem(&c.start));
+	/*
+	 * Unblock C which owns lock_c, and should deadlock attempting
+	 * to lock lock_a.
+	 */
+	__Tcall_assert(ret, evl_put_sem(&c.start));
+
+	__Texpr_assert(pthread_join(t_a, NULL) == 0);
+	__Texpr_assert(pthread_join(t_b, NULL) == 0);
+	__Texpr_assert(pthread_join(t_c, NULL) == 0);
 
 	evl_close_sem(&c.start);
 	evl_close_sem(&c.sync);
 	evl_close_mutex(&c.lock_a);
 	evl_close_mutex(&c.lock_b);
+	evl_close_mutex(&c.lock_c);
 
 	return 0;
 }
