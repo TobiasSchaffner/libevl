720a62f28c8c21d5d2525842c24ef8e0a3da5304
Philippe Gerum <rpm@xenomai.org>
2023-11-11 19:01:49 +0100

    lib/syscall: provide generic syscall wrapper
    EVL relies on Dovetail's handling of prctl(2) to receive requests
    which have the out-of-band syscall bit set in the option
    argument. Therefore we can provide a generic system call wrapper,
    dropping the arch-specific implementations in the same move.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- lib/syscall.c
+++ lib/syscall.c
@@ -7,8 +7,18 @@
 #include <stdarg.h>
 #include <pthread.h>
 #include <errno.h>
+#include <sys/prctl.h>
 #include <evl/syscall.h>
-#include <asm/evl/syscall.h>
+#include <asm-generic/dovetail.h>
+
+/*
+ * EVL relies on Dovetail's handling of prctl(2) to receive requests
+ * which have the out-of-band syscall bit set in the option
+ * argument. All EVL syscall return values fit in prctl's return type,
+ * which is a common integer.
+ */
+#define __evl_syscall(__nr, __a0, __a1, __a2)	\
+	prctl((__nr) | __OOB_SYSCALL_BIT, (long)(__a0), (long)(__a1), (long)(__a2), 0)
 
 ssize_t oob_read(int efd, void *buf, size_t count)
 {
@@ -16,12 +26,8 @@ ssize_t oob_read(int efd, void *buf, size_t count)
 	ssize_t ret;
 
 	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &old_type);
-	ret = evl_syscall3(sys_evl_read, efd, buf, count);
+	ret = __evl_syscall(sys_evl_read, efd, buf, count);
 	pthread_setcanceltype(old_type, NULL);
-	if (ret < 0) {
-		errno = -ret;
-		return -1;
-	}
 
 	return ret;
 }
@@ -32,12 +38,8 @@ ssize_t oob_write(int efd, const void *buf, size_t count)
 	ssize_t ret;
 
 	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &old_type);
-	ret = evl_syscall3(sys_evl_write, efd, buf, count);
+	ret = __evl_syscall(sys_evl_write, efd, buf, count);
 	pthread_setcanceltype(old_type, NULL);
-	if (ret < 0) {
-		errno = -ret;
-		return -1;
-	}
 
 	return ret;
 }
@@ -51,14 +53,9 @@ int oob_ioctl(int efd, unsigned long request, ...)
 	va_start(ap, request);
 	arg = va_arg(ap, long);
 	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &old_type);
-	ret = evl_syscall3(sys_evl_ioctl, efd, request, arg);
+	ret = __evl_syscall(sys_evl_ioctl, efd, request, arg);
 	pthread_setcanceltype(old_type, NULL);
 	va_end(ap);
 
-	if (ret) {
-		errno = -ret;
-		return -1;
-	}
-
-	return 0;
+	return ret;
 }
