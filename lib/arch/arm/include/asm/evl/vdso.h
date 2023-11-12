68b5a2343e192b5c5aeac4ae3cd14813065d18eb
Philippe Gerum <rpm@xenomai.org>
2023-11-12 11:00:18 +0100

    lib/arch: y2038: use 64bit vDSO interface to clock_gettime()
    Switch to the 64bit clock_gettime() interface in the vDSO when support
    for y2038 is enabled in the *libc.
    
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- lib/arch/arm/include/asm/evl/vdso.h
+++ lib/arch/arm/include/asm/evl/vdso.h
@@ -8,6 +8,11 @@
 #define _LIB_EVL_ARM_VDSO_H
 
 #define __EVL_VDSO_KVERSION	"LINUX_2.6"
+
+#ifdef __USE_TIME_BITS64
+#define __EVL_VDSO_GETTIME	"__vdso_clock_gettime64"
+#else
 #define __EVL_VDSO_GETTIME	"__vdso_clock_gettime"
+#endif
 
 #endif /* !_LIB_EVL_ARM_VDSO_H */
