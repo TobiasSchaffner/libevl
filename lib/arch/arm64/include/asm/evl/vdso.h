d98a0c26e02e7f8b4078064f2c4364e357f76e4d
Philippe Gerum <rpm@xenomai.org>
2024-07-31 15:52:27 +0200

    lib/arch: arm64: fix vDSO symbols
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- lib/arch/arm64/include/asm/evl/vdso.h
+++ lib/arch/arm64/include/asm/evl/vdso.h
@@ -9,10 +9,6 @@
 
 #define __EVL_VDSO_KVERSION	"LINUX_2.6.39"
 
-#ifdef __USE_TIME_BITS64
-#define __EVL_VDSO_GETTIME	"__vdso_clock_gettime64"
-#else
 #define __EVL_VDSO_GETTIME	"__kernel_clock_gettime"
-#endif
 
 #endif /* !_LIB_EVL_ARM64_VDSO_H */
