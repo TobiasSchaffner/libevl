77054f643f089edcee6fdb12b034f832f5f065e0
Philippe Gerum <rpm@xenomai.org>
2026-01-02 14:26:26 +0000

    libevl: require ABI 43
    Signed-off-by: Philippe Gerum <rpm@xenomai.org>

--- include/evl/evl.h
+++ include/evl/evl.h
@@ -25,7 +25,7 @@
 #define __EVL__  32	/* API version */
 
 /* The earliest ABI revision we can work with. */
-#define EVL_ABI_PREREQ  42
+#define EVL_ABI_PREREQ  43
 
 #if EVL_ABI_LEVEL < EVL_ABI_PREREQ
 #error EVL kernel uapi is too old
